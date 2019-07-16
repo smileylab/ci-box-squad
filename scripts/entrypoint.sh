24a9a8e976ce7813d5495eb71828a213390047da
Loic Poulain <loic.poulain@linaro.org>
2019-07-16 15:14:26 +0200

    change service starting order
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+sleep 20 # fixme
 /usr/sbin/rabbitmq-server &
 python3 /root/create_backend_lava.py &
 su squad
 squad --fast --bind 0.0.0.0:${SQUAD_PORT} &
 squad-worker &
-squad-listener &
-squad-scheduler
+squad-scheduler &
+squad-listener
